4a9b4758c7682d47e6bf135a3dca20da7b272beb
Michael Ilseman <milseman@apple.com>
2019-09-21 13:36:59 -0700

    Collection Consumers and Searchers Prototype

--- Package.swift
+++ Package.swift
@@ -1,34 +1,38 @@
 // swift-tools-version:5.1
-//===----------------------------------------------------------------------===//
-//
-// This source file is part of the Swift.org open source project
-//
-// Copyright (c) 2019 Apple Inc. and the Swift project authors
-// Licensed under Apache License v2.0 with Runtime Library Exception
-//
-// See https://swift.org/LICENSE.txt for license information
-// See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
-//
-//===----------------------------------------------------------------------===//
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-  name: "SE0000_PackageName",
-  products: [
-    .library(
-      name: "SE0000_PackageName",
-      targets: ["SE0000_PackageName"]),
-  ],
-  dependencies: [
-  ],
-  targets: [
-    .target(
-      name: "SE0000_PackageName",
-      dependencies: []),
-    
-    .testTarget(
-      name: "SE0000_PackageNameTests",
-      dependencies: ["SE0000_PackageName"]),
-  ]
+    name: "Prototype_CollectionConsumerSearcher",
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "Prototype_CollectionConsumerSearcher",
+            targets: ["Prototype_CollectionConsumerSearcher"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/ctxppc/PatternKit", .branch("development")),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "CollectionConsumerSearcher",
+            dependencies: []),
+        .testTarget(
+            name: "CollectionConsumerSearcherTests",
+            dependencies: ["CollectionConsumerSearcher"]),
+        .target(
+            name: "Prototype_CollectionConsumerSearcher",
+            dependencies: ["CollectionConsumerSearcher", "PatternKit"]),
+        .testTarget(
+            name: "PrototypeCollectionConsumerSearcherTests",
+            dependencies: ["Prototype_CollectionConsumerSearcher"]),
+        .target(
+            name: "Prototype_CollectionConsumerSearcherExample",
+            dependencies: ["Prototype_CollectionConsumerSearcher", "CollectionConsumerSearcher"]),
+    ]
 )
