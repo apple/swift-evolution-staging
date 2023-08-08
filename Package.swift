0d6c63a46a5bea315cac1a9a87312c9e6a25de86
Guillaume Lessard <guillaume.lessard@apple.com>
2023-08-08 09:04:18 -0700

    remove an unnecessary platform restriction

--- Package.swift
+++ Package.swift
@@ -5,7 +5,6 @@ import PackageDescription
 
 let package = Package(
     name: "SEnnnn_inputValidatingStringInitializers",
-    platforms: [.macOS(.v14)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
